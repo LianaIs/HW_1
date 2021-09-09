7adbcdd290db84be4f6d693d5a227eaaecf226f2
LianaIs <liana.isayan@mail.ru>
2021-09-09 04:23:29 +0400

    practical

---
+++ problem4.py.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+text = input('The given text: ')
+start_index = int(input("Start index: "))
+end_index = int(input("End index: "))
+print("Output string: " + text[start_index:end_index])
+
+
+# In[ ]:
+
+
+
+
